56216fef2e2e4c1db0af7c2a3bc905ef7ee41176
Froyder <homelik@mail.ru>
2021-04-25 19:38:28 +0300

    added onResult

--- app/src/main/java/com/example/calculatorintent/MainActivity.java
+++ app/src/main/java/com/example/calculatorintent/MainActivity.java
@@ -3,22 +3,45 @@ package com.example.calculatorintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_NUMBER = 99;
+    TextView tw;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.button).setOnClickListener(buttonListener);
+        TextView tw = findViewById(R.id.textView);
     }
 
     private View.OnClickListener buttonListener = v-> {
         Intent intent = new Intent(Intent.ACTION_RUN);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_NUMBER);
     };
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != REQUEST_NUMBER) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+
+        if (resultCode == RESULT_OK){
+            data.getParcelableExtra("NEW_NUMBER");
+            String number = data.getStringExtra("NEW_NUMBER");
+        //  et.setText(number);
+        }
+    }
 }
